d984778ead993194ad591fa795aa0074652ac56a
zrtch <1160639597@qq.com>
2021-06-21 22:52:16 +0800

    初始化项目

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { Fragment } from "react"
 
-function App(){
-    return(
-      <div className="App">
-      <h1>this is react app</h1>
-      </div>
+function App() {
+    return (
+        <Fragment>
+            <div className="App">
+                <h1>this is react app</h1>
+            </div>
+            <div className="App-other">this is react other app</div>
+        </Fragment>
     )
-  }
+}
 
-  export default App
+export default App
