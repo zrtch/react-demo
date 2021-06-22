d6a38e0eab7239e185197a50d355fae26dc33d7e
zrtch <1160639597@qq.com>
2021-06-22 22:44:22 +0800

    项目初始化

---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import "./home.scss"
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="p-home">
+                <h1>home page</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
